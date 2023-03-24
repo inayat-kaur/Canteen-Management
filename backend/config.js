ebb366d3a492ea033bdc25f01dae6d22e8d0f883
inayat-kaur <2020csb1088@iitrpr.ac.in>
2023-03-25 02:21:57 +0530

    update

---
+++ backend/config.js
@@ -0,0 +1,10 @@
+const config = {
+  db: {
+    host: "db4free.net",
+    user: "my_user",
+    password: "De.In.Ja.Po.Ta",
+    database: "canteen_app",
+  },
+  listPerPage: 10,
+};
+module.exports = config;

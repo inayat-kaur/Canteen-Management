07a8ac526f9814a2c12f98b4bf16c0bcfc8da4d4
Pooja-Goyal-1 <2020csb1108@iitrpr.ac.in>
2023-04-20 02:37:17 +0530

    urls updated

--- frontend/lib/urls.dart
+++ frontend/lib/urls.dart
@@ -1,37 +1,31 @@
-Uri addMenuItem = Uri.parse('http://localhost:8000/menu/addItem/');
-Uri deleteMenuItem(String id) =>
-    Uri.parse('http://localhost:8000/menu/deleteItem/$id');
-Uri updateItemPrice(String id) =>
-    Uri.parse('http://localhost:8000/menu/updatePrice/$id');
+String baseUrl = 'http://localhost:8000/';
+
+Uri addMenuItem = Uri.parse('${baseUrl}menu/addItem/');
+Uri deleteMenuItem(String id) => Uri.parse('${baseUrl}menu/deleteItem/$id');
+Uri updateItemPrice(String id) => Uri.parse('${baseUrl}menu/updatePrice/$id');
 Uri updateItemAvailability(String id) =>
-    Uri.parse('http://localhost:8000/menu/updateAvailability/$id');
-Uri updateItemRating(String id) =>
-    Uri.parse('http://localhost:8000/menu/updateRating/$id');
-Uri getMenu = Uri.parse('http://localhost:8000/menu/getMenu/');
+    Uri.parse('${baseUrl}menu/updateAvailability/$id');
+Uri updateItemRating(String id) => Uri.parse('${baseUrl}menu/updateRating/$id');
+Uri getMenu = Uri.parse('${baseUrl}menu/getMenu/');
 
-Uri addOrder = Uri.parse('http://localhost:8000/orders/addOrder/');
+Uri addOrder = Uri.parse('${baseUrl}orders/addOrder/');
 Uri deleteOrder(int id, String item) =>
-    Uri.parse('http://localhost:8000/orders/deleteOrder/$id/$item');
+    Uri.parse('${baseUrl}orders/deleteOrder/$id/$item');
 Uri updateOrderStatus(int id, String item) =>
-    Uri.parse('http://localhost:8000/orders/updateOrderStatus/$id/$item');
+    Uri.parse('${baseUrl}orders/updateOrderStatus/$id/$item');
 Uri updatePaymentStatus(int id, String item) =>
-    Uri.parse('http://localhost:8000/orders/updatePaymentStatus/$id/$item');
-Uri getOrders = Uri.parse('http://localhost:8000/orders/getOrders/');
-Uri getOrdersByUser(String id) =>
-    Uri.parse('http://localhost:8000/orders/deleteOrder/$id');
+    Uri.parse('${baseUrl}orders/updatePaymentStatus/$id/$item');
+Uri getOrders = Uri.parse('${baseUrl}orders/getOrders/');
+Uri getOrdersByUser(String id) => Uri.parse('${baseUrl}orders/deleteOrder/$id');
 Uri getOrdersByStatus(String status) =>
-    Uri.parse('http://localhost:8000/orders/getOrdersByStatus/$status');
+    Uri.parse('${baseUrl}orders/getOrdersByStatus/$status');
 
-Uri updateUserPhone(String id) =>
-    Uri.parse('http://localhost:8000/users/updatePhone/$id');
-Uri updateUserName(String id) =>
-    Uri.parse('http://localhost:8000/users/updateName/$id');
-Uri getUserProfile(String id) =>
-    Uri.parse('http://localhost:8000/users/getProfile/$id');
+Uri updateUserPhone(String id) => Uri.parse('${baseUrl}users/updatePhone/$id');
+Uri updateUserName(String id) => Uri.parse('${baseUrl}users/updateName/$id');
+Uri getUserProfile(String id) => Uri.parse('${baseUrl}users/getProfile/$id');
 
-Uri signUp = Uri.parse('http://localhost:8000/auth/signUp/');
-Uri login = Uri.parse('http://localhost:8000/auth/login/');
+Uri signUp = Uri.parse('${baseUrl}auth/signUp/');
+Uri login = Uri.parse('${baseUrl}auth/login/');
 
-Uri addCartItem = Uri.parse('http://localhost:8000/cart/addItem');
-Uri deleteCartItem(String id) =>
-    Uri.parse('http://localhost:8000/cart/deleteItem/$id');
+Uri addCartItem = Uri.parse('${baseUrl}cart/addItem');
+Uri deleteCartItem(String id) => Uri.parse('${baseUrl}cart/deleteItem/$id');

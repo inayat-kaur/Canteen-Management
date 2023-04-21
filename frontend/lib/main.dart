523abf99d1691abe7b119d05e928c8f32ff681ab
Deepika <2020csb1084@iitrpr.ac.in>
2023-04-22 02:04:53 +0530

    menupage

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/pages/menu_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: menuPage(),
     );
   }
 }
@@ -106,7 +107,13 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) =>  menuPage()),
+          );
+        },
+
         tooltip: 'Increment',
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
---
+++ frontend/lib/pages/category_menu_page.dart
@@ -0,0 +1,87 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/components/menuItem.dart';
+
+class CategoryMenuPage extends StatefulWidget {
+  final String category;
+
+  CategoryMenuPage({Key? key, required this.category}) : super(key: key);
+
+  @override
+  _CategoryMenuPageState createState() => _CategoryMenuPageState();
+}
+
+class _CategoryMenuPageState extends State<CategoryMenuPage> {
+  List<String> menuItems = [];
+
+  @override
+  void initState() {
+    super.initState();
+    // In this example, we're just hard-coding some menu items for each category
+    // You can replace this with your own data source or API call
+    if (widget.category == 'Image 1') {
+      menuItems = ['Fried Calamari', 'Bruschetta', 'Garlic Bread'];
+    } else if (widget.category == 'Entrees') {
+      menuItems = ['Spaghetti Bolognese', 'Chicken Parmesan', 'Pizza Margherita'];
+    } else if (widget.category == 'Desserts') {
+      menuItems = ['Tiramisu', 'Cannoli', 'Gelato'];
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.category),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              // Navigate to cart page
+              Navigator.push(
+                context,
+                MaterialPageRoute(builder: (context) =>  menuItem(
+                  imagePath: 'https://example.com/image.jpg',
+                  name: 'Item Name',
+                  price: 10,
+                  rating: 4,
+                  itemCount: 0,
+                )
+                ),
+              );
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.favorite),
+            onPressed: () {
+              // Navigate to wishlist page
+            },
+          ),
+          IconButton(
+            icon: Icon(Icons.person),
+            onPressed: () {
+              // Navigate to profile page
+            },
+          ),
+        ],
+      ),
+      body: menuItem(
+        imagePath: 'https://example.com/image.jpg',
+        name: 'Item Name',
+        price: 10,
+        rating: 4,
+        itemCount: 0,
+      )
+
+
+      // ListView.builder(
+      //   itemCount: menuItems.length,
+      //   itemBuilder: (BuildContext context, int index) {
+      //     return ListTile(
+      //       title: Text(menuItems[index]),
+      //       // Add any other menu item information, such as price, here
+      //     );
+      //   },
+      // ),
+    );
+  }
+}

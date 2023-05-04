b4eacd049ce0e770db07316f6f5bcb1cec953de1
Pooja-Goyal-1 <2020csb1108@iitrpr.ac.in>
2023-05-05 01:17:38 +0530

    unit tests - initial commit

--- frontend/lib/controllers/customer/cart_screen_controller.dart
+++ frontend/lib/controllers/customer/cart_screen_controller.dart
@@ -83,9 +83,14 @@ Future<List<Cart>> fetchCart() async {
 }
 
 Future<List<Product>> getProducts() async {
-  List<Product> foodProducts = [];
   List<Menu> menu = await fetchMenu();
   List<Cart> cart = await fetchCart();
+  List<Product> foodProducts = getProductsHelper(menu, cart);
+  return foodProducts;
+}
+
+List<Product> getProductsHelper(List<Menu> menu, List<Cart> cart) {
+  List<Product> foodProducts = [];
   for (int i = 0; i < cart.length; i++) {
     for (int j = 0; j < menu.length; j++) {
       if (cart[i].item == menu[j].item) {
